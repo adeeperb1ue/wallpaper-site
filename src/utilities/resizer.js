dc2a21c48d87f148ea9f99e118e6526dae0c8935
Jesse Sliter <sliterj@google.com>
2024-12-30 11:58:39 -0500

    added all flag to resize script

--- src/utilities/resizer.js
+++ src/utilities/resizer.js
@@ -5,6 +5,7 @@ const sizeOf = require("image-size")
 
 const inputDir = path.join(__dirname, '../image-manager/images/'); // Replace with your input directory
 const outputDir = path.join(__dirname, '../image-manager/thumbnails/'); // Replace with your output directory
+const all = process.argv.includes('all');
 
 const resizeImages = async () => {
   try {
@@ -13,15 +14,15 @@ const resizeImages = async () => {
     for (const file of files) {
       if(!file.endsWith(".png")) continue;
 
-      console.log(file);
       const inputPath = path.join(inputDir, file);
       const outputPath = path.join(outputDir, file.replace("png", "webp"));
       const dimensions = sizeOf(inputPath);
 
-      if(fs.existsSync(outputPath)){
-        console.log("already exists, skipping...");
+      if(fs.existsSync(outputPath) && !all){
         continue;
       }
+
+      console.log(file);
 
       await sharp(inputPath)
         .resize(dimensions.width / 2, dimensions.height / 2)
